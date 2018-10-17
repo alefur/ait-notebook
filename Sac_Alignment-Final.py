3c38539cbfadd2b1925fa6ec8c2ca36acd61d26d
Arnaud Le Fur <alefur@lam.fr>
2018-10-17 11:35:46 +0000

    concat dataframe

--- Sac_Alignment-Final.py
+++ Sac_Alignment-Final.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[7]:
+# In[1]:
 
 
 import sqlite3
@@ -14,26 +14,26 @@ from datetime import datetime as dt
 import numpy as np
 
 
-# In[8]:
+# In[2]:
 
 
 # import LAM library for logbook, data analysis...
 from pfs.sacFileHandling import *
 
 
-# In[9]:
+# In[3]:
 
 
 imgPath = '/home/pfs/shared/Pictures/SM1'
 
 
-# In[10]:
+# In[4]:
 
 
 smId = 1
 
 
-# In[11]:
+# In[5]:
 
 
 def correlate(reference, image):
@@ -46,8 +46,8 @@ def sacShift(filelist, duplicate):
     
     for i in range(len(filelist)//duplicate):
         hdr, image = stackedImage(filelist, i, duplicate=duplicate)
-        ccdPos = hdr['ait.sac.ccd.position']
-        pentaPos= hdr['ait.sac.penta.position']
+        ccdPos = round(hdr['ait.sac.ccd.position'], 1)
+        pentaPos= round(hdr['ait.sac.penta.position'], 1)
 
         shift, error, diffphase = correlate(reference=reference, image=image)
         res.append((ccdPos,pentaPos,shift[0], shift[1]))
@@ -72,63 +72,88 @@ def sacShift(filelist, duplicate):
 # ## Input Parameters : 
 # The only parameters needed is the experimentIds that match your data acquisition sequence
 
-# In[12]:
+# In[6]:
 
 
 experimentStart = 27 #the first experimentId is 12
 experimentEnd = 35    #the last experimentId is 16
 
 
-# In[13]:
+# In[7]:
 
 
-data = []
-
-plt.figure(figsize=(12,8))
-plt.xlabel('Pentaprism Position (mm)')
-plt.ylabel('offset (pixels)')
+dfs = []
 
 for experimentId in range(experimentStart, experimentEnd+1):
     visitStart, visitEnd = Logbook.visitRange(experimentId=experimentId)
-    focus = float(Logbook.getParameter(experimentId=experimentId, param='focus'))
     duplicate = int(Logbook.getParameter(experimentId=experimentId, param='duplicate'))
     
     filelist = constructFilelist(visitStart=visitStart, visitEnd=visitEnd)
+    dfs.append(sacShift(filelist=filelist, duplicate=duplicate))
 
-    df = sacShift(filelist=filelist, duplicate=duplicate)
-    plt.plot(df['pentaPosition'], df['shift_x'], 'o-', label='ccdPosition : %.2f'%df['ccdPosition'][0])
-    
-    [slope,off] = np.polyfit(df['pentaPosition'], df['shift_x'], deg=1)
-    data.append((focus, slope))
-    
-plt.title('Spot displacement vs the pentaprism position')
-plt.grid()
-plt.legend()
-
-doSave = False
-
-if doSave:
-    plt.savefig(os.path.join(imgPath, 'SM1_SACALIGN_EXP%i-%i_SPOT_DISPLACEMENT.png'%(experimentStart, experimentEnd)))
+cube = pd.concat(dfs)
 
 
 # In[8]:
 
 
-df = pd.DataFrame(data=data, columns=['focus', 'slope'])
+plt.figure(figsize=(12,8))
+plt.xlabel('Pentaprism Position (mm)')
+plt.ylabel('offset (pixels)')
+
+
+for ccdPosition, df in cube.groupby('ccdPosition'):
+    plt.plot(df['pentaPosition'], df['shift_x'], 'o-', label='ccdPosition : %.2f'%ccdPosition)
+    
+plt.title('Spot displacement vs pentaPosition')
+plt.grid()
+plt.legend()
+
+if False:
+    plt.savefig(os.path.join(imgPath, 'SM1_SACALIGN_EXP%i-%i_SPOT_DISPLACEMENT.png'%(experimentStart, experimentEnd)))
 
 
 # In[9]:
 
 
-x = np.arange(np.min(df['focus']), np.max(df['focus'])+0.01, 0.01)
-popt = np.polyfit(df['focus'], df['slope'], deg=1)
+plt.figure(figsize=(12,8))
+plt.xlabel('Ccd Position (mm)')
+plt.ylabel('offset (pixels)')
 
+
+for pentaPosition, df in cube.groupby('pentaPosition'):
+    plt.plot(df['ccdPosition'], df['shift_x'], 'o-', label='pentaPosition : %.2f'%pentaPosition)
+    
+plt.title('Spot displacement vs ccdPosition')
+plt.grid()
+plt.legend()
+
+if False:
+    plt.savefig(os.path.join(imgPath, 'SM1_SACALIGN_EXP%i-%i_SPOT_DISPLACEMENT.png'%(experimentStart, experimentEnd)))
+
+
+# In[10]:
+
+
+data = []
+for ccdPosition, df in cube.groupby('ccdPosition'):
+    [slope, offset] = np.polyfit(df['pentaPosition'], df['shift_x'], deg=1)
+    data.append((ccdPosition, slope))
+    
+df = pd.DataFrame(data=data, columns=['ccdPosition', 'slope'])
+
+
+# In[11]:
+
+
+x = np.arange(np.min(df['ccdPosition']), np.max(df['ccdPosition'])+0.01, 0.01)
+popt = np.polyfit(df['ccdPosition'], df['slope'], deg=1)
 
 plt.figure(figsize=(12,8))
 plt.xlabel('Ccd Position (mm)')
 plt.ylabel('Slope')
 
-plt.plot(df['focus'], df['slope'], 'o')
+plt.plot(df['ccdPosition'], df['slope'], 'o')
 plt.plot(x, np.polyval(popt, x), '-')
 
 calc_focus = -popt[1]/popt[0]
@@ -136,6 +161,29 @@ plt.vlines(x=calc_focus, ymin=np.min(df['slope']), ymax=np.max(df['slope']))
 plt.title('Calculated Focus = %.3f mm'%calc_focus)
 plt.grid()
 
-if doSave:
+if False:
     plt.savefig(os.path.join(imgPath, 'SM1_SACALIGN_EXP%i-%i_CALC_FOCUS.png'%(experimentStart, experimentEnd)))
+
+
+# In[12]:
+
+
+f = 1497.
+pix_size = 4.539e-3
+foc = []
+
+for ccdPosition, df in cube.groupby('ccdPosition'):
+    foc.append(ccdPosition+df.shift_x.iloc[-1] * pix_size * f / 90.)
+
+
+# In[13]:
+
+
+plt.figure(figsize=(12,8))
+plt.xlabel('Ccd Position (mm)')
+plt.ylabel('Calculated Focus(mm)')
+plt.title('Focus other method')
+
+plt.plot(foc, 'o-')
+plt.grid()
 
